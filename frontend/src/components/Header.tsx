3ff7a628fb1ea9d0730d4e2c39a3cf7cff85af2a
Jaya Rohith Vemula <jayarohithvemula@gmail.com>
2025-06-25 18:48:50 -0500

    feat: Make MyPortfolio title clickable to navigate to LandingPage

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -13,7 +13,13 @@ export default function Header() {
   return (
     <AppBar position="static" sx={{ backgroundColor: grey[900] }}>
       <Toolbar className="flex justify-between">
-        <Typography variant="h6">MyPortfolio</Typography>
+        <Typography
+          variant="h6"
+          sx={{ cursor: "pointer" }}
+          onClick={() => navigate("/landingpage")}
+        >
+          MyPortfolio
+        </Typography>
         <Box>
           <Button color="inherit" onClick={handleLogout}>
             Logout
